979d7db3ea502ab908dc31c4e263ea2336e2fe4f
monailing88@gmail.com <monailing88@gmail.com>
2013-07-25 21:16:43 -0400

    added layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '1df45f5ef7540b05696879e2f286ad84ce5b999e8af92fb88a63497a20fcba69b4f0958f84b21c9cdfccf69aec4767503f8e447dc0c177abe96595b0696ac83e'
